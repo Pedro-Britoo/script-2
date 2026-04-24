d8e95f024635b797800267ce296820316cd9c01b
Britto <166552093+Pedro-Britoo@users.noreply.github.com>
2026-04-24 20:11:24 -0300

    Implement countdown from 5 to 1
    Removed comments and implemented countdown from 5 to 1.

--- Exer01.cs
+++ Exer01.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class Exer01 : MonoBehaviour
 {
-    //1. (Contagem regressiva de tempo) Crie uma contagem
-    //regressiva de 5 a 1 e exiba "Início da partida!" ao final.
+    
     void Start()
     {
-        //for(inicializador; condição; incremento/decremento) 
+        
         for(int i = 5; i > 0; i--)
         {
             print(i);
         }
-        print("Início da partida!");
+        print("InÃ­cio da partida!");
     }
 
     
